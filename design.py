1494f7191eb771b9e1536fe5d6cf05c9b25a3cc6
Emilio Mendoza <52044603+Ironislife98@users.noreply.github.com>
2022-12-21 09:19:17 -0500

    Working on more frames

--- design.py
+++ design.py
@@ -1,13 +1,16 @@
 import tkinter
 import customtkinter
 import json
-
+from typing import Callable
 
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-DEFAULT_SETTINGS = {"Command": {"description": "This tool allows you to run commands on a remote server and see the output from the remote server",
-                                "warnings": ""}}
+DEFAULT_SETTINGS = {"Command": {
+    "description": "This tool allows you to run commands on a remote server and see the output from the remote server",
+    "warnings": ""}}
+
+layouts = []
 
 try:
     with open("settings.json") as f:
@@ -19,26 +22,52 @@ except FileNotFoundError:
 
 
 # Custom CTk Frames
-class commandFrame(customtkinter.CTkFrame):
-    def __init__(self, *args, headerName="",  **kwargs):
+class Frame(customtkinter.CTkFrame):    # Just here to avoid repeating adding layouts
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+        layouts.append(self)
+
+
+class commandFrame(Frame):
+    def __init__(self, *args, headerName="", onsubmit: Callable, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.headerName = headerName
 
-        self.frameTitle = customtkinter.CTkLabel(self, text=self.headerName, font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.frameTitle.grid(row=0, column=0)
+        self.frameTitle = customtkinter.CTkLabel(self, text=self.headerName,
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.frameTitle.grid(row=0, column=1, padx=(200, 0))
 
         self.description = SETTINGS["Command"]["description"]
-        print(self.description)
+        self.descriptionLabel = customtkinter.CTkLabel(self, text=self.description, font=customtkinter.CTkFont(size=15))
+        self.descriptionLabel.grid(row=1, column=1, padx=(200, 0))
+
+        self.outputBox = customtkinter.CTkTextbox(self, state="disabled")
+        self.outputBox.grid(row=2, column=1, sticky="nsew", padx=(20, 0))
+        self.outputBox.insert("0.0", "No Output...")
+
+        self.commandEntry = customtkinter.CTkEntry(self, placeholder_text="Enter Command")
+        self.commandEntry.grid(row=3, column=0, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+        self.runCommandButton = customtkinter.CTkButton(master=self, text="Run Command", fg_color="transparent",
+                                                        border_width=2,
+                                                        text_color=("gray10", "#DCE4EE"), command=onsubmit)
+        self.runCommandButton.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
 
+class screenshotFrame(Frame):
+    def __init__(self, *args, ontakeScreenshot: Callable, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# Main Application Class
 class App(customtkinter.CTk):
     def __init__(self) -> None:
         super().__init__()
 
         self.title("PyRemote")
         self.geometry(f"{1100}x{580}")
-        self.resizable(False, False)
+        # self.resizable(False, False)
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
@@ -51,27 +80,44 @@ class App(customtkinter.CTk):
         self.sidebarFrame.grid_rowconfigure(4, weight=1)
 
         # Sidebar Labels and Buttons
-        self.sidebarHeader = customtkinter.CTkLabel(self.sidebarFrame, text="Hello", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.sidebarHeader = customtkinter.CTkLabel(self.sidebarFrame, text="PyRemote",
+                                                    font=customtkinter.CTkFont(size=20, weight="bold"))
         self.sidebarHeader.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        self.commandButton = customtkinter.CTkButton(self.sidebarFrame, text="Run Command", command=self.switchLayoutCommand)
+        self.commandButton = customtkinter.CTkButton(self.sidebarFrame, text="Run Command",
+                                                     command=self.switchLayoutCommand)
         self.commandButton.grid(row=1, column=0, padx=20, pady=10)
 
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebarFrame)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebarFrame, text="Screenshot Viewer", command=self.switchLayoutScreenshot)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
 
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebarFrame)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
 
         # Create main frame
-        self.mainFrame = commandFrame(self, headerName="Run Commands")
+        self.mainFrame = commandFrame(self, headerName="Run Commands", onsubmit=self.sendCommand)
         self.mainFrame.grid(row=0, column=1, padx=20, pady=20)
 
-        self.mainFrameButton = customtkinter.CTkButton(self, text="Print value of frame 2")
-        self.mainFrameButton.grid(row=1, column=1, padx=20, pady=10)
+        # Create Screenshot frame
+        self.ssFrame = screenshotFrame(self, ontakeScreenshot=self.switchLayoutScreenshot)
+        #self.ssFrame.grid(row=0, column=1, padx=20, pady=20)
+
+    # Going to start a new connection every time
+    # One thread will remain open for other connection stuff
+    def sendCommand(self):
+        pass
 
     def switchLayoutCommand(self) -> None:
-        pass
+        self.clearLayout()
+        self.mainFrame.grid(row=0, column=1, padx=20, pady=20)
+
+    def switchLayoutScreenshot(self):
+        self.clearLayout()
+        self.ssFrame.grid(row=0, column=1, padx=20, pady=20)
+
+    def clearLayout(self):
+        for layout in layouts:
+            layout.grid_forget()
 
 
 if __name__ == "__main__":
