12dfefd7411ab224cf1bc8dc027bbfe2bf8fffb1
EmilioMendoza <Ironislife39@gmail.com>
2022-12-21 18:13:48 -0500

    Added GUI for screenshot and camera capture

--- design.py
+++ design.py
@@ -2,6 +2,8 @@ import tkinter
 import customtkinter
 import json
 from typing import Callable
+from PIL import Image
+
 
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -58,6 +60,29 @@ class commandFrame(Frame):
 class screenshotFrame(Frame):
     def __init__(self, *args, ontakeScreenshot: Callable, **kwargs):
         super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
+        self.screenshot = customtkinter.CTkImage(light_image=Image.open("images/placeholder.png"),
+                                          dark_image=Image.open("images/placeholder.png"),
+                                          size=(400, 200))
+        self.screenshotContainerLabel = customtkinter.CTkLabel(self, image=self.screenshot, text="")
+        self.screenshotContainerLabel.grid(row=0, column=0)
+
+        self.takeScreenshot = customtkinter.CTkButton(self, text="Take Screenshot", command=ontakeScreenshot)
+        self.takeScreenshot.grid(row=1, column=0)
+
+
+class cameraFrame(Frame):
+    def __init__(self, *args, onCapture: Callable, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
+        self.capture = customtkinter.CTkImage(light_image=Image.open("images/placeholder.png"),
+                                          dark_image=Image.open("images/placeholder.png"),
+                                          size=(400, 200))
+        self.captureContainerLabel = customtkinter.CTkLabel(self, image=self.capture, text="")
+        self.captureContainerLabel.grid(row=0, column=0)
+
+        self.takeCapture = customtkinter.CTkButton(self, text="Take Camera Capture", command=onCapture)
+        self.takeCapture.grid(row=1, column=0)
 
 
 # Main Application Class
@@ -76,7 +101,7 @@ class App(customtkinter.CTk):
 
         # Create sidebar
         self.sidebarFrame = customtkinter.CTkFrame(self, width=200, corner_radius=0)
-        self.sidebarFrame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebarFrame.grid(row=0, column=0, rowspan=6, sticky="nsew")
         self.sidebarFrame.grid_rowconfigure(4, weight=1)
 
         # Sidebar Labels and Buttons
@@ -88,19 +113,24 @@ class App(customtkinter.CTk):
                                                      command=self.switchLayoutCommand)
         self.commandButton.grid(row=1, column=0, padx=20, pady=10)
 
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebarFrame, text="Screenshot Viewer", command=self.switchLayoutScreenshot)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebarFrame, text="Screen Capture", command=self.switchLayoutScreenshot)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
 
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebarFrame)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebarFrame, text="CameraCapture",
+                                                        command=self.switchLayoutScreenshot)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebarFrame)
+        self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
 
         # Create main frame
         self.mainFrame = commandFrame(self, headerName="Run Commands", onsubmit=self.sendCommand)
         self.mainFrame.grid(row=0, column=1, padx=20, pady=20)
 
-        # Create Screenshot frame
-        self.ssFrame = screenshotFrame(self, ontakeScreenshot=self.switchLayoutScreenshot)
-        #self.ssFrame.grid(row=0, column=1, padx=20, pady=20)
+        # Create Screenshot frame and Camera Capture Frame
+        self.ssFrame = screenshotFrame(self, ontakeScreenshot=self.takeScreenshot)
+        # self.ssFrame.grid(row=0, column=1, padx=20, pady=20)
+        self.cameraCaptureFrame = cameraFrame(self, onCapture=self.takeCapture)
 
     # Going to start a new connection every time
     # One thread will remain open for other connection stuff
@@ -114,11 +144,18 @@ class App(customtkinter.CTk):
     def switchLayoutScreenshot(self):
         self.clearLayout()
         self.ssFrame.grid(row=0, column=1, padx=20, pady=20)
+        self.cameraCaptureFrame.grid(row=0, column=2, padx=20, pady=20)
 
     def clearLayout(self):
         for layout in layouts:
             layout.grid_forget()
 
+    # Placeholder funcs
+    def takeScreenshot(self):
+        pass
+
+    def takeCapture(self):
+        pass
 
 if __name__ == "__main__":
     app = App()
